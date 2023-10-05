895f5c79e6a6999866f1c805111319062d7332df
pragathiswar <110533918+pragathiswar@users.noreply.github.com>
2023-10-05 15:56:03 +0530

    Add files via upload

---
+++ Day-12/src/test/java/com/example/demo/travelapp/Traex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Traex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
